440b5edcecffa45e2f826830aa6a7f484abba2ba
fragomo <francisco.gonzalezmorales66@gmail.com>
2024-09-30 12:31:30 +0100

    Modificacion Score

--- score.js
+++ score.js
@@ -1,13 +1,29 @@
+//import player from "./player.js";
+
+
 let scoreText;
 let score = 0;
 let count = 0;
+//let coin = 50;
+
+function setCoin() {
+  let coin = document.getElementById("coin");
+  coin = 50;
+}
 
 function scoreSet() {
   scoreText = document.getElementById("score");
+  setCoin();
+  let coin = document.getElementById("coin");
   if (player == true) {
-    count += coin.value;
+    count += coin;
+    score = count;
   }
 
   scoreText.innerHTML = `Score: ${score}`;
 }
+
+let player;
 scoreSet();
+
+
