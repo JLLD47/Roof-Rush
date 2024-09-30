bb4c7c9b0ae3a0a0965b5ccdde326660f7daf39c
fragomo <francisco.gonzalezmorales66@gmail.com>
2024-09-30 15:51:36 +0100

    commit

--- obstacle.js
+++ obstacle.js
@@ -1,19 +1,16 @@
 class Obstacle {
-    constructor(x, y, width, height) {
+    constructor() {
         this.x = 50;
         this.y = 50;
         this.width = 10;
         this.height = 20; 
-        this.sprite = document.createlement("div");    
+        this.sprite = document.createElement("div");    
         this.interval = setInterval(this.sprite(), 20) 
-    }
-    draw() {
-        this.fillStyle = "red";
-        this.fillRect(this.x, this.y, this.width, this.height);
     }
     insert() {
        this.sprite.position = "absolute";
        playfield.appendChild(this.sprite);
+       this.sprite.setAttribute("id", "obstacle");
     }
 }
 
