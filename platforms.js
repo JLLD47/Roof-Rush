6534136edf37e6114bd0bf56f6d01d24aa39bfde
DankestDev <DankestMirror@hotmail.es>
2024-09-30 11:43:35 +0100

    Metodo de Insert de la clase Platforms creado

--- platforms.js
+++ platforms.js
@@ -5,5 +5,16 @@ class Platforms {
         this.height
         this.x
         this.y
+        this.directionX = -1 // Izquierda por default
+        this.speed = 1
+        this.sprite = document.createElement('div')
+    }
+    insert(){
+        this.sprite.setAttribute('class', 'platform')
+        this.sprite.style.width = this.width + 'em'
+        this.sprite.style.height = this.height + 'em'
+        this.sprite.style.top = this.y + 'em'
+        this.sprite.style.left = this.x + 'em'
+        playfield.appendChild(this.sprite)
     }
 }
